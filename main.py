e7f0fa469388fc3877ca8f226fd6911cf7197b41
Juan B <juanbasora89@gmail.com>
2022-05-07 15:38:26 -0400

    URL error checking.
    Added a try except on the user input to verify the url is a valid best buy url.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from getter import Getter
 
+
 def main():
-    g = Getter()
-    user_input = input("Enter the best buy URL: \n")
-    g.getProductData()
-    
+    getter = Getter()
+    user_input = input("Enter the best buy URL: ")
+    if "bestbuy" in user_input.split() and "skuId=" in user_input.split():
+        getter.getProductData(user_input)
+    else:
+        print("Please enter a valid Best Buy ulr.")
 
 if __name__ == '__main__':
     main()
