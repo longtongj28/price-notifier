6beba2dd48c10df26e8fc7a7fdb25593b35aad93
Juan B <juanbasora89@gmail.com>
2022-05-09 20:16:50 -0400

    Add files via upload
    Added a url verifier to make sure the url provided is a valid url. 
    Verifier added to requirements.
    Added checks the url is from bestbuy and checks the url for a skuId.

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from getter import Getter
-
+from validators import url
 
 def main():
     getter = Getter()
     user_input = input("Enter the best buy URL: ")
-    if "bestbuy" in user_input.split() and "skuId=" in user_input.split():
+    valid_url = url(user_input)
+    bb_url = user_input.__contains__("bestbuy")
+    bb_item = user_input.__contains__("skuId")
+
+    if valid_url and bb_url and bb_item:
         getter.getProductData(user_input)
     else:
-        print("Please enter a valid Best Buy ulr.")
+        print("Please verify the url.")
 
 if __name__ == '__main__':
     main()
